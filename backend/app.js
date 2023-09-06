89182b0f2a95e7473e6b275368ba082575406019
ducanhly <namobilegame@gmail.com>
2023-09-06 21:40:36 +0700

    push

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://react-kazl.onrender.com/"],
+    origin: "https://react-kazl.onrender.com",
     credentials: true,
   })
 );
