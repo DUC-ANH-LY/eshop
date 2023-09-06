c80169c3fb74792399df9415f33846497087b12c
ducanhly <namobilegame@gmail.com>
2023-09-06 21:06:32 +0700

    push

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,6 @@
-export const server = "http://localhost:8000/api/v2";
+// export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "http://localhost:8000/"
+// export const backend_url = "http://localhost:8000/"
+export const server = "https://mern-stack-npr9.onrender.com/api/v2";
+
+export const backend_url = "https://mern-stack-npr9.onrender.com/"
